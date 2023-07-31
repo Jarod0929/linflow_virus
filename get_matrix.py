b3132dcaf95e06c7e52b3f0f911957512dc1e51a
Jarod0929 <JarodLe@icloud.com>
2023-07-31 12:00:27 -0400

    Final Commit Working get_matrix.py Uncommented the things

--- get_matrix.py
+++ get_matrix.py
@@ -1,9 +1,63 @@
 import pandas as pd
 import numpy as np
-import editdistance
+import sys
+from Bio import Align
 
 import os
 import sys
+
+def get_mismatch(alignment, seq1, seq2):
+    tup = alignment[0].aligned
+    pos1 = 0
+    pos2 = 0
+    mis = 0
+
+    if len(tup[0]) == 0 or len(tup[1]) == 0:
+        return 0
+    
+    t1 = tup[0][pos1][0]
+    t2 = tup[1][pos2][0]
+
+    while(True):
+
+        if (t1 >= tup[0][pos1][1]):
+            pos1 += 1
+            if (pos1 >= len(tup[0])):
+                break
+            t1 = tup[0][pos1][0]
+        
+        if (t2 >= tup[1][pos2][1]):
+            pos2 += 1
+            if (pos2 >= len(tup[1])):
+                break
+            t2 = tup[1][pos2][0]
+
+        if (seq1[t1] != seq2[t2]):
+            mis += 1
+
+        t1 += 1
+        t2 += 1
+        
+    return mis
+
+def get_gaps(alignment, seq1, seq2):
+    tup = alignment[0].aligned
+
+    gap1 = 0
+    prev = 0
+    for i in range(len(tup[0])):
+        gap1 = tup[0][i][0] - prev + gap1
+        prev = tup[0][i][1]
+
+    gap2 = 0
+    prev = 0
+    for i in range(len(tup[1])):
+        gap2 = tup[1][i][0] - prev + gap2
+        prev = tup[1][i][1]
+
+    ex_gap = abs(len(seq1) - len(seq2)) - abs(gap1 - gap2)
+
+    return gap1 + gap2 + ex_gap
 
 def main(country):
     #CREATED NEW ENV with pyani
@@ -13,7 +67,7 @@ def main(country):
     cmd = "unzip " + country + "/inter/filtered_" + country + "_fastas.zip"
     os.system(cmd)
 
-    #pyani
+    # #pyani
 
     cmd = "mkdir " + country + "/inter/ANIm_dir"
     os.system(cmd)
@@ -21,7 +75,7 @@ def main(country):
     cmd = "average_nucleotide_identity.py -i " + country + "/inter/filtered_fastas/ -o " + country + "/inter/ANIm_dir -m ANIm -g -f"
     os.system(cmd)
 
-    cmd = "mv " + country + "/inter/ANIm_dir/ANIm_similarity_errors.tab " + country + "/inter/"
+    cmd = "mv " + country + "/inter/ANIm_dir/ANIm_percentage_identity.tab " + country + "/inter/"
     os.system(cmd)
 
     cmd = "rm -r " + country + "/inter/ANIm_dir"
@@ -29,7 +83,7 @@ def main(country):
 
     # #edit distance
 
-    df_ANIm = pd.read_csv(country + "/inter/ANIm_similarity_errors.tab", sep='\t')
+    df_ANIm = pd.read_csv(country + "/inter/ANIm_percentage_identity.tab", sep='\t')
 
     df_ANIm.rename(columns={df_ANIm.columns[0]:"genome_file"}, inplace=True)
 
@@ -44,31 +98,48 @@ def main(country):
         seq =""
         for line in f:
             if (line[0] != ">"):
-                seq = seq + line
+                seq = seq + line.replace('\n', '')
         sequences.append(seq)
 
-    for col in range(1,len(df_ed.columns)+1):
+    aligner = Align.PairwiseAligner()
+
+    aligner.mode = 'global'
+
+    aligner.match_score = 2
+    aligner.mismatch_score = -3
+
+    aligner.open_gap_score = -5
+    aligner.extend_gap_score = -2
+    aligner.target_end_gap_score = 0.0
+    aligner.query_end_gap_score = 0.0
+
+    for col in range(1,len(df_ed.columns)):
         name_col = df_ed.columns[col]
 
-        for row in range(1,col):
+        for row in range(1,col+1):
             name_row = df_ed.columns[row]
             print(col, row)
 
             if col == row:
                 val = 0
             else:
-                val = editdistance.eval(sequences[col-1], sequences[row-1])
+                seq1 = sequences[col-1]
+                seq2 = sequences[row-1]
+
+                alignment = aligner.align(seq1, seq2)
+                val = get_mismatch(alignment, seq1, seq2) + get_gaps(alignment, seq1, seq2)
+
             df_ed[name_col][row-1] = val
             df_ed[name_row][col-1] = val
 
-    df_ed.to_csv(country + "/inter/editdistance.csv", index=False)
+    df_ed.to_csv(country + "/inter/global_alignment.csv", index=False)
 
     cmd = "rm -r " + country + "/inter/filtered_fastas"
     os.system(cmd)
 
     #combining two matrices
-    df_ANIm = pd.read_csv(country + "/inter/ANIm_similarity_errors.tab", sep='\t')
-    df_ed = pd.read_csv(country + "/inter/editdistance.csv")
+    df_ANIm = pd.read_csv(country + "/inter/ANIm_percentage_identity.tab", sep='\t')
+    df_ed = pd.read_csv(country + "/inter/global_alignment.csv")
 
     df = pd.DataFrame(columns = df_ANIm.columns)
     df_ANIm.rename(columns={df_ANIm.columns[0]:"genome_file"}, inplace=True)
